e56093ebe111cd3fbcdb752902a8c68539696ab4
Isaias257 <85814646+Isaias257@users.noreply.github.com>
2022-05-29 18:00:11 -0700

    Add files via upload

---
+++ SolarProject_DataCollection.ino
@@ -0,0 +1,89 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+ 
+#define ONE_WIRE_BUS 8 // Data wire is plugged into port 8 on the Arduino
+#define precision 12 // OneWire precision Dallas Sensor
+int sen_number = 0; // Counter of Dallas sensors
+ 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+DeviceAddress T1, T2, T3, T4, T5; // arrays to hold device addresses
+void setup(void)
+{
+lcd.begin(16,2);
+Serial.begin(9600); //Start serial port
+
+// Start up the library
+sensors.begin();
+// locate devices on the bus
+// report parasite power requirements
+// Search for devices on the bus and assign based on an index.
+// show the addresses we found on the bus
+
+// set the resolution to 12 bit per device
+sensors.setResolution(T1, precision);
+sensors.setResolution(T2, precision);
+sensors.setResolution(T3, precision);
+sensors.setResolution(T4, precision);
+sensors.setResolution(T5, precision);
+
+
+}
+// function to print a device address
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+float tempC = sensors.getTempC(deviceAddress);
+Serial.print(tempC);
+// Serial.print(" Temp F: ");
+// Serial.print(DallasTemperature::toFahrenheit(tempC));
+}
+// function to print a device's resolution
+void printResolution(DeviceAddress deviceAddress)
+{
+}
+ 
+void printData(DeviceAddress deviceAddress)
+{
+printTemperature(deviceAddress);
+Serial.println();
+}
+ 
+void loop(void)
+{
+// call sensors.requestTemperatures() to issue a global temperature request to all devices on the bus
+sensors.requestTemperatures();
+// print the device information
+for (int k =0; k < sensors.getDeviceCount(); k++) {
+Serial.print("Sensor"); Serial.print(k+1); Serial.print(" ");
+if (k == 0) { printData(T1);
+} else if (k == 1) { printData(T2);
+} else if (k == 2) { printData(T3);
+} else if (k == 3) { printData(T4);
+} else if (k == 4) { printData(T5);
+}
+}
+if (sen_number == sensors.getDeviceCount()) {
+sen_number = 0; // reset counter
+// lcd.clear(); // clear screen on LCD
+}
+lcd.setCursor(0,0);
+lcd.print("Sensor Number ");
+lcd.print(sen_number+1);
+lcd.setCursor(0,1);
+lcd.print(" Temp: ");
+if (sen_number == 0) { lcd.print(sensors.getTempC(T1)); lcd.write((char)223); lcd.print("C ");
+} else if (sen_number == 1) { lcd.print(sensors.getTempC(T2)); lcd.write((char)223); lcd.print("C ");
+} else if (sen_number == 2) { lcd.print(sensors.getTempC(T3)); lcd.write((char)223); lcd.print("C ");
+} else if (sen_number == 3) { lcd.print(sensors.getTempC(T4)); lcd.write((char)223); lcd.print("C ");
+} else if (sen_number == 4) { lcd.print(sensors.getTempC(T5)); lcd.write((char)223); lcd.print("C ");
+}
+
+delay(1000);
+sen_number++ ;
+}
